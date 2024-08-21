291305ae49576100e05b316feb5488410fe9deec
EduardoMonti <dudumonti@hotmail.com>
2024-08-21 17:06:35 -0300

    Adding H2 bank configuration

--- src/main/java/com/example/MedicineRegistrationApplication.java
+++ src/main/java/com/example/MedicineRegistrationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedicineRegistrationApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MedicineRegistrationApplication.class, args);
 	}
 
