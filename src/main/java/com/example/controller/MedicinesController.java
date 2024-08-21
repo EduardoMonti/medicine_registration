83ec1ef418b5709b861cea4cd644a062a6f3ace2
EduardoMonti <dudumonti@hotmail.com>
2024-08-21 18:06:39 -0300

    Including findAll and findById in MedicineController

--- src/main/java/com/example/controller/MedicinesController.java
+++ src/main/java/com/example/controller/MedicinesController.java
@@ -1,2 +1,32 @@
-package com.example.controller;public class MedicinesController {
+package com.example.controller;
+
+import com.example.entities.Medicines;
+import com.example.repository.MedicinesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/medicines")
+public class MedicinesController {
+
+    @Autowired
+    MedicinesRepository medicinesRepository;
+
+    @GetMapping
+    public List<Medicines> findAll(){
+        List<Medicines> medicines = medicinesRepository.findAll();
+        return medicines;
+    }
+
+    @GetMapping(value = "/{id}")
+    public Medicines findById(@PathVariable Long id){
+        Medicines medicines = medicinesRepository.findById(id).get();
+        return  medicines;
+    }
+
 }
