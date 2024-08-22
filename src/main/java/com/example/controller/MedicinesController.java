b3c5b804c40f20b6ba4e2f3516a3c09f8debf54a
EduardoMonti <dudumonti@hotmail.com>
2024-08-22 15:42:25 -0300

    Including delete by id in MedicineController

--- src/main/java/com/example/controller/MedicinesController.java
+++ src/main/java/com/example/controller/MedicinesController.java
@@ -3,12 +3,11 @@ package com.example.controller;
 import com.example.entities.Medicines;
 import com.example.repository.MedicinesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/medicines")
@@ -27,6 +26,19 @@ public class MedicinesController {
     public Medicines findById(@PathVariable Long id){
         Medicines medicines = medicinesRepository.findById(id).get();
         return  medicines;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Medicines> deleteById(@PathVariable Long id){
+        Optional<Medicines> medicinesOptional = medicinesRepository.findById(id);
+
+        if (medicinesOptional.isPresent()) {
+            Medicines medicines = medicinesOptional.get();
+            medicinesRepository.deleteById(id);
+            return ResponseEntity.ok(medicines);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
